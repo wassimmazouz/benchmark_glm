ba0976acadff639ae5b7b9446f7994e2fd9ec115
wassimmazouz <wassimmazouz5261@gmail.com>
2024-05-17 10:59:17 +0200

    normalized diabetes dataset

--- datasets/diabetes.py
+++ datasets/diabetes.py
@@ -5,7 +5,7 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     from sklearn.datasets import load_diabetes
-    from sklearn.preprocessing import LabelBinarizer
+    import numpy as np
 
 
 class Dataset(BaseDataset):
@@ -17,5 +17,11 @@ class Dataset(BaseDataset):
 
     def get_data(self):
         diabetes = load_diabetes()
-        X, y = diabetes.data, diabetes.target
+        X1, y1 = diabetes.data, diabetes.target
+
+        mean = np.mean(X1, axis=0)
+        std = np.std(X1, axis=0)
+
+        X = (X1 - mean) / std
+        y = y1
         return dict(X=X, y=y)
