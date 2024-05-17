ca0cd28cb5e7507324b247f349f19ae18ec74660
wassimmazouz <wassimmazouz5261@gmail.com>
2024-05-17 15:45:28 +0200

    added iris dataset for multilogreg

---
+++ datasets/iris.py
@@ -0,0 +1,34 @@
+# This python file loads the iris dataset from sklearn.
+# This dataset is intended for the multilogreg model
+
+from benchopt import BaseDataset
+
+from benchopt import safe_import_context
+
+
+with safe_import_context() as import_ctx:
+    from sklearn.datasets import load_iris
+    from sklearn.preprocessing import OneHotEncoder
+    from sklearn.model_selection import train_test_split
+
+
+class Dataset(BaseDataset):
+
+    name = "iris"
+
+    install_cmd = 'conda'
+    requirements = ['scikit-learn']
+
+    def get_data(self):
+        iris = load_iris()
+        X1, y1 = iris.data, iris.target
+
+        # Convert labels to one-hot encoding
+        encoder = OneHotEncoder()
+        y2 = encoder.fit_transform(y1.reshape(-1, 1)).toarray()
+
+        X, X_test, y, y_test = train_test_split(X1, y2, test_size=0.2, random_state=42)
+
+        data = dict(X=X, y=y)
+
+        return data
