9ac5daf4fa6974acde5c3f14c9c46ef3530b6232
wassimmazouz <wassimmazouz5261@gmail.com>
2024-05-21 15:22:53 +0200

    added new logreg dataset

---
+++ datasets/breast_cancer.py
@@ -0,0 +1,26 @@
+from benchopt import BaseDataset
+
+from benchopt import safe_import_context
+
+
+with safe_import_context() as import_ctx:
+    from sklearn.datasets import load_breast_cancer
+    from sklearn.preprocessing import LabelBinarizer
+    import numpy as np
+
+
+class Dataset(BaseDataset):
+
+    name = "bcancer"
+
+    install_cmd = 'conda'
+    requirements = ['scikit-learn']
+
+    def get_data(self):
+        X, y = load_breast_cancer(return_X_y=True)
+        X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+
+        binarizer = LabelBinarizer(neg_label=-1, pos_label=1)
+        y = binarizer.fit_transform(y)[:, 0].astype(X.dtype)
+
+        return dict(X=X, y=y)
