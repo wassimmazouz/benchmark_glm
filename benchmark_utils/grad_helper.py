0f5ed4b0e3c8ce5693f1fcdbff62e536eec69468
wassimmazouz <wassimmazouz5261@gmail.com>
2024-05-29 11:42:54 +0200

    removed comments

--- benchmark_utils/grad_helper.py
+++ benchmark_utils/grad_helper.py
@@ -7,8 +7,5 @@ def gradient_linreg(X, y, beta):
 
 
 def gradient_logreg(X, y, beta):
-    # return X.T @ (expit(X @ beta) - y)
     y_X_beta = y * (X @ beta)
-    # temp = 1. / (1. + np.exp(ywTx))
-    # grad = -(X.T @ (y * temp))
     return -(X.T @ (y * expit(-y_X_beta)))
