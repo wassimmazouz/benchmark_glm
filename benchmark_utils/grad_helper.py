FIX overflow in exp and modified gradient_logreg

benchmark_utils/grad_helper.py
@@ -1,4 +1,5 @@
 from scipy.special import expit
+import numpy as np
 
 
 def gradient_linreg(X, y, beta):
@@ -6,4 +7,8 @@ def gradient_linreg(X, y, beta):
 
 
 def gradient_logreg(X, y, beta):
-    return X.T @ (expit(X @ beta) - y)
+    # return X.T @ (expit(X @ beta) - y)
+    y_X_beta = y * (X @ beta)
+    # temp = 1. / (1. + np.exp(ywTx))
+    # grad = -(X.T @ (y * temp))
+    return -(X.T @ (y * expit(-y_X_beta)))

benchmark_utils/obj_helper.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import expit
 
 
 def objective_function_linreg(X, y, beta):
@@ -8,4 +9,4 @@ def objective_function_linreg(X, y, beta):
 
 def objective_function_logreg(X, y, beta):
     y_X_beta = y * (X @ beta)
-    return np.log1p(np.exp(-y_X_beta)).sum()
+    return -np.log(expit(y_X_beta)).sum()